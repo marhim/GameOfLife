c3852f0965b65fe7449a890b5dcb42cd1d0805dd
Marvin Himmelmeier <arusaki@gmx-topmail.de>
2016-08-21 00:32:21 +0200

    Expanded LivingSpace-class and its tests

--- src/main/java/de/arusaki/gameoflife/model/LivingSpace.java
+++ src/main/java/de/arusaki/gameoflife/model/LivingSpace.java
@@ -12,10 +12,65 @@ public class LivingSpace {
     private final int rows;
     private final boolean[][] livingSpace;
 
+    /**
+     * Initializes the living space fields with given number of columns and rows.
+     *
+     * @param columns number of columns the living space should have
+     * @param rows    number of rows the living space should have
+     */
     public LivingSpace(int columns, int rows) {
         this.columns = columns;
         this.rows = rows;
         this.livingSpace = new boolean[columns][rows];
+    }
+
+    /**
+     * Set the Cell at given position alive.
+     *
+     * @param posX x-position
+     * @param posY y-position
+     * @return true, if setting cell alive at given position, false if not
+     */
+    public boolean reviveCellAt(int posX, int posY) {
+        try {
+            livingSpace[posX][posY] = true;
+        } catch (ArrayIndexOutOfBoundsException ignored) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Checks status of the cell at given position. {@link IndexOutOfBoundsException} will be thrown, if position is out of bounds.
+     *
+     * @param posX x-position
+     * @param posY y-position
+     * @return true, if Cell is alive, false if not
+     */
+    public boolean isAliveAt(int posX, int posY) {
+        return livingSpace[posX][posY];
+    }
+
+    /**
+     * Counts living neighbors of given position except itself. {@link IndexOutOfBoundsException} will be thrown, if position is out of bounds.
+     *
+     * @param posX x-position
+     * @param posY y-position
+     * @return count of living neighbors
+     */
+    public int countLivingNeighborsAt(int posX, int posY) {
+        int livingNeighborsCount = 0;
+        for (int i = posX - 1; i < posX + 2; i++) {
+            for (int j = posY - 1; j < posY + 2; j++) {
+                // ignore given position and check out of bounds, if so continue with next iteration
+                if (!((i == posX && j == posY) || i < 0 || j < 0 || i >= columns || j >= rows)) {
+                    if (livingSpace[i][j]) {
+                        livingNeighborsCount++;
+                    }
+                }
+            }
+        }
+        return livingNeighborsCount;
     }
 
     public int getColumns() {
@@ -24,9 +79,5 @@ public class LivingSpace {
 
     public int getRows() {
         return rows;
-    }
-
-    public boolean[][] getLivingSpace() {
-        return livingSpace;
     }
 }
--- src/test/java/de/arusaki/gameoflife/test/model/LivingSpaceTest.java
+++ src/test/java/de/arusaki/gameoflife/test/model/LivingSpaceTest.java
@@ -5,8 +5,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 /**
  * Testclass for methods from {@link LivingSpace}.
@@ -16,11 +15,14 @@ import static org.junit.Assert.assertNotNull;
  */
 public class LivingSpaceTest {
 
+    private static final int COUNT_COLUMNS = 4;
+    private static final int COUNT_ROWS = 3;
+
     private LivingSpace livingSpace3x2;
 
     @Before
     public void setUp() {
-        livingSpace3x2 = new LivingSpace(3, 2);
+        livingSpace3x2 = new LivingSpace(COUNT_COLUMNS, COUNT_ROWS);
     }
 
     @After
@@ -34,14 +36,76 @@ public class LivingSpaceTest {
     }
 
     @Test
-    public void testSizeOfColumnsInInitialization() {
-        assertEquals("LivingSpace-Array must have 3 columns", 3, livingSpace3x2.getLivingSpace().length);
-        assertEquals("getColumns() must have 3 columns", 3, livingSpace3x2.getColumns());
+    public void testSizeOfColumnsIsCorrect() {
+        assertEquals("getColumns() must have " + COUNT_COLUMNS + " columns", COUNT_COLUMNS, livingSpace3x2.getColumns());
     }
 
     @Test
-    public void testSizeOfRowsInInitialization() {
-        assertEquals("LivingSpace-Array must have 2 rows", 2, livingSpace3x2.getLivingSpace()[0].length);
-        assertEquals("getRows() must have 2 rows", 2, livingSpace3x2.getRows());
+    public void testSizeOfRowsIsCorrect() {
+        assertEquals("getRows() must have " + COUNT_ROWS + " rows", COUNT_ROWS, livingSpace3x2.getRows());
     }
+
+    @Test
+    public void testReviveAtSuccessful() {
+        assertTrue("revive at (0, 0) must return true", livingSpace3x2.reviveCellAt(0, 0));
+    }
+
+    @Test
+    public void testReviveAtFailure() {
+        assertFalse("revive at (-1, -1) must return false", livingSpace3x2.reviveCellAt(-1, -1));
+    }
+
+    @Test
+    public void testIsCellAtPositionAliveFalse() {
+        assertFalse("isAlive() at (0, 0) must be false", livingSpace3x2.isAliveAt(0, 0));
+    }
+
+    @Test
+    public void testIsCellAtPositionAliveTrue() {
+        livingSpace3x2.reviveCellAt(0, 0);
+        assertTrue("isAlive() at (0, 0) must be true", livingSpace3x2.isAliveAt(0, 0));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testIsAliveAtOutOfBounds() {
+        livingSpace3x2.isAliveAt(-1, -1);
+    }
+
+    @Test
+    public void testCountLivingNeighborsAtTopLeftCorner() {
+        livingSpace3x2.reviveCellAt(0, 0);
+        livingSpace3x2.reviveCellAt(1, 1);
+        assertEquals("living neighbors count must be 1", 1, livingSpace3x2.countLivingNeighborsAt(0, 0));
+    }
+
+    @Test
+    public void testCountLivingNeighborsAtTopLeftCornerWithEveryCellAroundAlive() {
+        livingSpace3x2.reviveCellAt(0, 0);
+        livingSpace3x2.reviveCellAt(0, 1);
+        livingSpace3x2.reviveCellAt(1, 0);
+        livingSpace3x2.reviveCellAt(1, 1);
+        assertEquals("living neighbors count must be 3", 3, livingSpace3x2.countLivingNeighborsAt(0, 0));
+    }
+
+    @Test
+    public void testCountLivingNeighborsInMiddle() {
+        livingSpace3x2.reviveCellAt(0, 0);
+        livingSpace3x2.reviveCellAt(1, 1);
+        assertEquals("living neighbors count must be 1", 1, livingSpace3x2.countLivingNeighborsAt(1, 1));
+    }
+
+    @Test
+    public void testCountLivingNeighborsInMiddleWithEveryCellAroundAlive() {
+        livingSpace3x2.reviveCellAt(0, 0);
+        livingSpace3x2.reviveCellAt(0, 1);
+        livingSpace3x2.reviveCellAt(0, 2);
+        livingSpace3x2.reviveCellAt(1, 0);
+        livingSpace3x2.reviveCellAt(1, 1);
+        livingSpace3x2.reviveCellAt(1, 2);
+        livingSpace3x2.reviveCellAt(2, 0);
+        livingSpace3x2.reviveCellAt(2, 1);
+        livingSpace3x2.reviveCellAt(2, 2);
+        assertEquals("living neighbors count must be 8", 8, livingSpace3x2.countLivingNeighborsAt(1, 1));
+    }
+
 }
